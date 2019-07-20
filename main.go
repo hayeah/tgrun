3a16b4af3850e1504580f787135d5f9d747f3d92
Howard Yeh <hayeah@gmail.com>
2019-07-20 19:24:38 +0800

    update process uptime

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"log"
 	"os"
 	"os/exec"
+	"os/signal"
 	"strconv"
 	"time"
 
@@ -37,48 +38,119 @@ func run() error {
 		return err
 	}
 
-	host, err := os.Hostname()
-	if err != nil {
-		return err
-	}
-
 	args := os.Args[1:]
 
 	if len(args) == 0 {
 		return errors.New("must specify a command to run")
 	}
 
+	host, err := os.Hostname()
+	if err != nil {
+		return err
+	}
+
 	cmd := args[0]
 	args = args[1:]
 
-	c := exec.Command(cmd, args...)
+	bot, err := tgbotapi.NewBotAPI(token)
+
+	r := runner{
+		Host:   host,
+		ChatID: int64(chatID),
+		Bot:    bot,
+		Cmd:    exec.Command(cmd, args...),
+	}
+
+	err = r.start()
+	return err
+}
+
+type runner struct {
+	Host   string
+	ChatID int64
+	Bot    *tgbotapi.BotAPI
+	Cmd    *exec.Cmd
+}
+
+func (r *runner) start() error {
+	go func() {
+		err := r.updateStatus()
+		if err != nil {
+			log.Println(err)
+		}
+	}()
+
+	go r.handleInterrupt()
+
+	return r.runCommand()
+}
+
+func (r *runner) runCommand() error {
+	c := r.Cmd
+
 	c.Stderr = os.Stderr
 	c.Stdout = os.Stdout
 	c.Stdin = os.Stdin
 
-	err = c.Start()
+	err := c.Start()
 	if err != nil {
 		return err
 	}
-
-	start := time.Now()
 
 	c.Wait()
 
-	elapsed := time.Now().Sub(start)
+	_, err = r.sendMessage("Exit status: %d", c.ProcessState.ExitCode())
+	return err
+}
 
-	bot, err := tgbotapi.NewBotAPI(token)
-	if err != nil {
-		return err
+func (r *runner) handleInterrupt() {
+	sigc := make(chan os.Signal)
+	signal.Notify(sigc, os.Interrupt)
+
+	for sig := range sigc {
+		log.Println("SIGINT, exit now", sig)
 	}
+}
 
-	msgtxt := fmt.Sprintf("[%s] `%s` exited with status %d\nElapsed: %s", host, cmd, c.ProcessState.ExitCode(), elapsed)
+func (r *runner) Tag() string {
+	return fmt.Sprintf("[%s] %d `%s`", r.Host, r.Cmd.Process.Pid, r.Cmd.Args[0])
+}
 
-	msg := tgbotapi.NewMessage(int64(chatID), msgtxt)
-	_, err = bot.Send(msg)
-	if err != nil {
-		return err
+func (r *runner) sendMessage(format string, a ...interface{}) (tgbotapi.Message, error) {
+	s := fmt.Sprintf("%s\n%s", r.Tag(), fmt.Sprintf(format, a...))
+	return r.Bot.Send(tgbotapi.NewMessage(r.ChatID, s))
+}
+
+func (r *runner) editMessage(msgid int, format string, a ...interface{}) (tgbotapi.Message, error) {
+	s := fmt.Sprintf("%s\n%s", r.Tag(), fmt.Sprintf(format, a...))
+	return r.Bot.Send(tgbotapi.NewEditMessageText(r.ChatID, msgid, s))
+}
+
+func (r *runner) updateStatus() error {
+	// i := 0
+	start := time.Now()
+
+	var m tgbotapi.Message
+	for {
+		var err error
+
+		time.Sleep(2 * time.Second)
+		elapsed := time.Now().Sub(start)
+
+		if m.MessageID == 0 {
+			m, err = r.sendMessage("Uptime: %s", elapsed)
+
+			if err != nil {
+				return err
+			}
+
+			continue
+		}
+
+		_, err = r.editMessage(m.MessageID, "Uptime: %s", elapsed)
+
+		if err != nil {
+			return err
+		}
 	}
-
-	return nil
 }
